0a0494cd3ec086973b73c2b684eb4a6d35b7b196
ahsanullah875 <au7125616@gmail.com>
2025-11-15 10:43:15 +0500

    some thing added

---
+++ Assignment 01 (alert & prompt)/script.js
@@ -0,0 +1,21 @@
+// Assignment 01 (alert & prompt)
+
+// solution 01
+
+// var username = prompt("Please enter your name:");
+// alert("Hello, " + username );
+
+// solution 02
+// var username = prompt("Please enter your name:");
+// var age = prompt("Please enter your age:");
+// console.log (username + " is " + age + " years old.");
+
+// solution 03
+// var color = prompt("Please enter your favorite color:");
+// alert("wow! " + color + " is a nice color!");
+
+// solution 04
+var num1 = prompt("Please enter a number");
+var num2 = prompt("Please enter another number");
+console.log("The result of" + num1 + num2 + "is" );
+
