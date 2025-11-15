ef2b6a549fd5dc6df278dcf8fed97fbdd135bc86
ahsanullah875 <au7125616@gmail.com>
2025-11-15 12:01:05 +0500

    javascripts operators

--- Assignment 01 (alert & prompt)/script.js
+++ Assignment 01 (alert & prompt)/script.js
@@ -15,7 +15,14 @@
 // alert("wow! " + color + " is a nice color!");
 
 // solution 04
-var num1 = prompt("Please enter a number");
-var num2 = prompt("Please enter another number");
-console.log("The result of" + num1 + num2 + "is" );
 
+// var num1 = prompt("Enter first number:");
+// var num2 = prompt("Enter second number:");
+// console.log("The result of " +  num1 + " + " + num2 + " is " + (Number(num1) + Number(num2)) );
+
+// solution 05
+
+// var username = prompt("Please enter your name:");
+// var country = prompt("Please enter your country:");
+// var hobby = prompt("Please enter your hobby:");
+// alert(" Hi, I'm " + username + " from " + country + " and I Love " + hobby + "!");
