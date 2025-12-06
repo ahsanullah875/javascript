006213e9f940622c524cffcc9c5debca4d05c8d0
ahsanullah875 <au7125616@gmail.com>
2025-12-06 11:14:33 +0500

    arry

---
+++ Arry/script.js
@@ -0,0 +1,29 @@
+// array methods
+
+//push
+//pop
+//unshift
+//shift
+//splice
+//slice
+//split
+//concat
+//sort
+//reverse
+//indexof
+
+
+
+// let arr = ["world", 123, "hello", true, "khan", false];
+// arr.push("ahsan")
+// arr.pop(false)
+// arr.unshift("ahsan")
+// arr.shift()
+// arr.splice(1,2)
+// console.log(arr);
+let cities = ["Quetta", "Lahore", "Islamabad","Karachi","Gilgit"]
+console.log(cities);
+let newarry=cities.slice(1,3)
+console.log(newarry);
+
+// cities.splice(1,3)
---
+++ Functions/script.js
@@ -0,0 +1,152 @@
+// function resuablecode(params) {
+//     console.log("This is reusable code");
+//     console.log("this is sesuable code");
+//     console.log("This is reusable code");
+//     console.log("this is sesuable code");
+//     console.log("This is reusable code");
+//     console.log("this is sesuable code");
+// }
+// resuablecode(); Function call
+// resuablecode();
+
+// function greetuser(username, age) {
+//     console.log("Hello " + username + age + ", welcome back!");
+
+// }
+// greetuser("Ahsan " , 25);
+
+// function sum(num1, num2) {
+//     console.log(num1 + num2);
+// }
+// sum(5, 10);
+// sum(20, 30);
+// sum(100, 200);
+
+// function sum(num1, num2) {
+//     return num1 + num2;
+// }
+// let total = sum(20, 30);
+// console.log(total);
+
+// function percent(username,num1,num2,num3) {
+//     let totalmarks = 300;
+//     let obtainedmarks = num1 + num2 + num3;
+//     let percentage = (obtainedmarks / totalmarks) * 100;
+//  return username + " your percentage is " + percentage + "%"; 
+// }
+// console.log(percent("Ahsan ", 85, 90, 80));
+// console.log(percent("Ali ", 70, 75, 80));
+// console.log(percent("Ahmed ", 60, 65, 70));
+
+// let head = document.querySelector("#head");
+// function change() {
+
+//     head.innerHTML = "Hello";
+// }
+// change();
+
+
+// const head = document.querySelector("#head");
+// function greetuser(){
+//     console.log("hello world");
+//     head.innerHTML = "change"
+
+// }
+
+// function change(name) {
+// console.log("Hello " + name);
+// console.log("Hello " + name);
+// console.log("Hello " + name);
+// console.log("Hello " + name);
+// console.log("Hello " + name);
+// console.log("Hello " + name);
+// }
+// change("Ahsan");
+
+// function sum(a , b, c=5) {
+// return a + b + c;
+
+
+// }
+// result1 = sum(5, 10);
+// result2 = sum(20, 30);
+// result3 = sum(100, 200);
+// console.log("The result is " + result1);
+// console.log("The result is " + result2);
+// console.log("The result is " + result3);
+
+
+
+// function myname(name) {         parameter
+//     console.log(name);
+
+// }
+// myname("Ahsan");                argument
+
+
+// sum of two numbers
+
+// function sum(a,b) {
+//     console.log(a+b);
+
+// }
+// sum(5,10);
+
+// function sum(a , b) { parmeters are local variables
+// s= a + b;
+// return s;
+// }
+// let val = sum(5,10);
+// console.log(val);
+
+
+// const h1 = document.querySelector("#head");
+// const p = document.querySelector("#para");
+// function chang(){
+//     if (h1.innerHTML==="Hello World")
+        
+    
+//     {
+//        h1.innerHTML="change throw javascript" 
+//     }else{
+//         h1.innerHTML="Hello World"
+//     }
+// }
+
+
+
+// // function change() {
+    // // h1.innerHTML="change throw javascript"
+    // // }
+
+    //     const p =document.querySelector("#para")
+    //     const h1=document.querySelector("#head")
+//     function change() {
+//     if (h1.innerHTML==="Hello World",
+//         p.innerHTML==="this is para"
+//     ) {
+//     h1.innerHTML="change throw javascript" 
+//     p.innerHTML="my name is ahsan"
+        
+// }else{
+//     h1.innerHTML="Hello World"
+//     p.innerHTML="this is para"
+// }
+// }
+
+
+
+// arry
+
+// let arr =["world",123,"hello",true,"khan",false]
+// console.log(arr[4]);
+// arr[4]="ahsan";
+// arr.push(13)
+// console.log(arr);
+
+// arr.pop()
+
+const h1 = document.querySelector("#head");
+h1.innerHTML="hi world"
+const p = document.querySelector("#para");
+p.innerHTML="this is paragraph"
